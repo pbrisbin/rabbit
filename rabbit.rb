0198495771a454aba3c4e177057955867c289e75
patrick brisbin <pbrisbin@gmail.com>
2011-08-21 13:59:26 -0400

    a bit more usable

--- rabbit.rb
+++ rabbit.rb
@@ -2,14 +2,35 @@
 #
 # rabbit. an aur-helper in ruby.
 #
+# This is a toy. There is currently little to no:
+#
+#   * Features
+#   * Error handling
+#   * Configuration
+#
 ###
+require_relative 'lib/aursearch'
 require_relative 'lib/package'
 
-pkg = Package.find 'aurget'
+case ARGV[0]
+when '-S'
+  pkg = Package.find ARGV[1]
 
-if pkg
-  pkg.download
-  pkg.extract
-  pkg.build
-  pkg.install
+  if pkg
+    pkg.download
+    pkg.extract
+    pkg.build
+    pkg.install
+  end
+
+when '-Ss'
+  srch = AurSearch.new ARGV[1]
+  srch.show_results
+
+when '-Ssi'
+  srch = AurSearch.new ARGV[1], :info
+  srch.show_results
+
+else
+  puts "usage: rabbit [ -S <pkg> | -Ss <term> | -Ssi <term> ]"
 end
