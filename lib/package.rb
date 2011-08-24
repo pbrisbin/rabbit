0c2a21e9060dab39adac0aa90e47a7946d2a6bc7
patrick brisbin <pbrisbin@gmail.com>
2011-08-24 14:50:11 -0400

    being configuration logic

--- lib/package.rb
+++ lib/package.rb
@@ -1,5 +1,19 @@
+require 'fileutils'
 require 'open-uri'
 require_relative 'aursearch'
+
+# an error that should stop processing of that target only
+class RabbitNonError < StandardError
+end
+
+# a build error on a dependecy must stop the processesing of its parent
+# and any other deps left to build
+class RabbitBuildError < StandardError
+end
+
+# an error that should stop us entirely
+class RabbitError < StandardError
+end
 
 class Package
   attr_reader :name, :version, :base_url, :archive
@@ -21,17 +35,33 @@ class Package
   end
 
   def self.install *targets
+    # create the build dir if needed
+    unless Dir.exists? $config.build_directory
+      begin FileUtils.mkdir_p $config.build_directory
+      rescue
+        raise RabbitError, "Could not create #{$config.build_directory}."
+      end
+    end
+
+    # change to it
+    Dir.chdir $config.build_directory
+
     targets.each do |target|
       pkg = self.find target
 
       if pkg
         begin
           pkg.download
-          pkg.extract
-          pkg.build
-          pkg.install
-        rescue
-          STDERR.puts "Something broke installing #{pkg.name}"
+          pkg.extract unless $config.sync_level < 1
+          pkg.build   unless $config.sync_level < 2
+          pkg.install unless $config.sync_level < 3
+
+        rescue RabbitNonError => e
+          STDERR.puts e.message, "Skipping #{pkg.name}."
+
+        rescue RabbitError => e
+          STDERR.puts e.message
+          exit 1
         end
       end
     end
@@ -43,30 +73,25 @@ class Package
     begin
       a = open(archive_url)
       f = open(@archive, "wb")
-
       f.write(a.read)
-
-      a.close
-      f.close
     rescue
-      STDERR.puts "Error downloading the package"
-
+      raise RabbitNonError, "Error downloading the package"
+    ensure
       a.close
       f.close
-
-      raise
     end
   end
 
   def extract
     if File.exists? @archive
       unless system "tar xzf \"#{@archive}\""
-        STDERR.puts "Tar threw an error"
-        raise
+        raise RabbitNonError, "Tar threw an error"
       end
+
+      # maybe discard the taball
+      File.delete @archive if $config.discard_tarball
     else
-      STDERR.puts "#{archive}: file not found"
-      raise
+      raise RabbitNonError, "#{archive}: file not found"
     end
   end
 
@@ -76,13 +101,18 @@ class Package
       Dir.chdir @name
 
       if File.exists? 'PKGBUILD'
-        unless system "makepkg"
-          STDERR.puts "Makepkg threw an error"
-          raise
+        unless system $config.makepkg
+          raise RabbitNonError "Makepkg threw an error"
+        end
+
+        # maybe discard the sources
+        if $config.discard_sources
+          Dir.glob("./*") do |fp|
+            FileUtils.rm_rf fp unless fp =~ /.*\.pkg\.tar\.[gx]z$/
+          end
         end
       else
-        STDERR.puts "PKGBUILD: file not found"
-        raise
+        raise RabbitNonError, "PKGBUILD: file not found"
       end
 
       Dir.chdir oldpwd
@@ -102,13 +132,45 @@ class Package
       args = to_install.collect { |a| "'#{a}'" }.join(' ')
 
       # install all of them
-      unless system "sudo pacman -U #{args}"
-        STDERR.puts "Pacman threw an error"
-        raise
+      unless system "#{$config.pacman} #{args}"
+        raise RabbitNonError, "Pacman threw an error"
+      end
+
+      if $config.discard_package
+        to_install.each { |pkg| File.delete pkg }
+      else
+        unless Dir.exists? $config.package_directory
+          begin FileUtils.mkdir_p $config.package_directory
+          rescue
+            to_install = [] # so we don't try to save them
+            raise RabbitNonError, "Could not create #{$config.package_directory}."
+          end
+        end
+
+        to_install.each do |pkg|
+          begin
+            from = open(pkg, "rb")
+            to   = open("#{$config.package_directory}/#{File.basename pkg}", "wb")
+            to.write(from.read)
+
+            File.delete pkg
+          rescue
+            raise RabbitNonError, "Error saving the package"
+          ensure
+            from.close
+            to.close
+          end
+        end
+
+        if Dir.exists? @name
+          begin Dir.delete @name
+          rescue Errno::ENOTEMPTY
+            # silenty ignore
+          end
+        end
       end
     else
-      STDERR.puts "No packages found"
-      raise
+      raise RabbitNonError, "No packages found to install"
     end
   end
 end
--- rabbit.rb
+++ rabbit.rb
@@ -12,8 +12,37 @@
 require_relative 'lib/aursearch'
 require_relative 'lib/package'
 
+class Config
+  attr_reader :pacman, :makepkg, :sync_level,
+    :build_directory, :package_directory,
+    :discard_sources, :discard_tarball, :discard_package,
+    :resolve_deps, :edit_pkgbuilds, :ignore_packages
+
+  # the default config. options not implemented are commented for now
+  def initialize
+    @pacman            = "sudo pacman -U"
+    @makepkg           = "makepkg"
+    @sync_level        = 3 # 0 => download, 1 => extract, 2 => build, 3 => install
+    @build_directory   = ENV['HOME'] + "/Sources"
+    @package_directory = ENV['HOME'] + "/Packages"
+    @discard_sources   = true
+    @discard_tarball   = true
+    @discard_package   = false
+    #@resolve_deps      = false
+    #@edit_pkgbuilds    = :always #, :never, or :prompt
+    #@ignore_packages   = []
+  end
+
+  def load_config_file fp
+    # todo
+  end
+end
+
+$config = Config.new
+$config.load_config_file "/etc/rabbit.yml"
+
 case ARGV.shift
-  when '-S' ; Package.install *ARGV
   when '-Ss'; AurSearch.search *ARGV
   when '-Si'; AurSearch.info  *ARGV
+  when '-S' ; Package.install *ARGV
 end
