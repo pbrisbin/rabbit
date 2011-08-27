22578523425e9ffdda505a5cf632751061bd1307
patrick brisbin <pbrisbin@gmail.com>
2011-08-27 14:08:52 -0400

    refactor pkgbuild into package, add config and sample yml

--- lib/package.rb
+++ lib/package.rb
@@ -20,6 +20,42 @@ end
 class RabbitError < StandardError
 end
 
+class Pkgbuild
+  attr_reader :depends, :makedepends
+
+  # argument is the pkgbuild contents as a string
+  def initialize pkgbuild
+    @pkgbuild = pkgbuild
+  end
+
+  def parse!
+    @depends     = parse_bash_array :depends
+    @makedepends = parse_bash_array :makedepends
+  end
+
+  private
+
+  def parse_bash_array varname
+    if @pkgbuild =~ /(^|\s)#{varname.to_s}=\((.*?)\)/m
+      # remove inline comments, join multiline statements, split on
+      # whitespace, pull out just the package name from a variety of
+      # quoting and/or version bounds
+      items = $2.split(/#.*?\n/m).join.split(/[\s]+/).collect do |item|
+        if item =~ /("|')([^><=]*)[><=]{0,2}.*\1/
+          $2
+        else
+          item
+        end
+      end
+
+      items.delete ""
+      items
+    else
+      []
+    end
+  end
+end
+
 class Package
   attr_reader :name, :version, :base_url, :archive
 
--- rabbit.rb
+++ rabbit.rb
@@ -9,36 +9,12 @@ $LOAD_PATH << # current directory and ./lib
   File.dirname(Pathname.new(File.expand_path(__FILE__)).realpath) <<
   File.dirname(Pathname.new(File.expand_path(__FILE__)).realpath) + '/lib'
 
+require 'config'
 require 'aursearch'
 require 'package'
-require 'pkgbuild'
-
-class Config
-  attr_reader :pacman, :makepkg, :sync_level,
-    :build_directory, :package_directory,
-    :discard_sources, :discard_tarball, :discard_package,
-    :resolve_deps, :edit_pkgbuilds, :ignore_packages
-
-  def initialize
-    @pacman            = "sudo pacman -U"
-    @makepkg           = "makepkg -s --nocolor"
-    @sync_level        = 3
-    @build_directory   = ENV['HOME'] + "/Sources"
-    @package_directory = ENV['HOME'] + "/Packages"
-    @discard_sources   = true
-    @discard_tarball   = true
-    @discard_package   = false
-    @resolve_deps      = true
-    @edit_pkgbuilds    = :never
-    @ignore_packages   = []
-  end
-
-  def load_config_file fp
-    # todo: yaml?
-  end
-end
 
 $config = Config.new
+$config.load_config_file
 
 case ARGV.shift
   when '-Ss'; AurSearch.search   *ARGV
