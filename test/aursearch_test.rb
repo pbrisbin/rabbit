62c96a84d59312a4dc265e76d349c8b18a590a95
patrick brisbin <pbrisbin@gmail.com>
2011-09-16 14:35:50 -0400

    make test work

--- test/aursearch_test.rb
+++ test/aursearch_test.rb
@@ -1,6 +1,6 @@
 require 'test/unit'
 
-require "#{File.dirname(__FILE__)}/../lib/aursearch"
+require_relative'../lib/aursearch'
 
 class AurTest < Test::Unit::TestCase
   def test_rpc_search_with_one_result
@@ -17,35 +17,25 @@ class AurTest < Test::Unit::TestCase
   end
 
   def test_rpc_search_with_no_results
-    begin
-      json = Aur.call_rpc(:search, 'xlkdjowqiehgoqiiweg')
-      assert false, "Should've returned not found"
-    rescue RabbitNotFoundError
-      assert true # expected
-    end
+    assert_raise(RabbitNotFoundError) { json = Aur.call_rpc(:search, 'xlkdjowqiehgoqiiweg') }
   end
 
   def test_rpc_info_single
     json = Aur.call_rpc(:multiinfo, 'aurget')
-    assert json_has_key?('results')
+    assert json.has_key?('results')
     assert_equal json['results'].length, 1
     assert_equal json['results'].first['Name'], 'aurget'
   end
 
   def test_rpc_info_multiple
     json = Aur.call_rpc(:multiinfo, 'aurget cower-git')
-    assert json_has_key?('results')
+    assert json.has_key?('results')
     assert_equal json['results'].length, 2
     assert_equal json['results'][0]['Name'], 'aurget'
-    assert_equal json['results'][2]['Name'], 'cower-git'
+    assert_equal json['results'][1]['Name'], 'cower-git'
   end
 
   def test_rpc_invalid_type
-    begin
-      x = Aur.call_rpc(:foo, 'bar')
-      assert false, "Should've returned an error"
-    rescue RabbitError
-      assert true # expected error
-    end
+    assert_raise(RabbitError){ x = Aur.call_rpc(:foo, 'bar') }
   end
 end
