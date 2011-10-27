4cee8d846a6a9aa80ace71d818f8d95ed5e0390f
patrick brisbin <pbrisbin@gmail.com>
2011-10-26 23:50:42 -0400

    begin major rewrite

--- lib/rabbit/json.rb
+++ lib/rabbit/json.rb
@@ -3,52 +3,37 @@ require 'json'
 require 'net/http'
 require 'threaded-each'
 
-module JsonResult
-  RPC='http://aur.archlinux.org/rpc.php'
-  KEY='results'
+module Rabbit
+  module Json
+    KEY='results'
 
-  def json_url(type, term)
-    return "#{RPC}?type=multiinfo#{multi_search_arg(term)}" if [:info, :pkgbuild].include?(type)
-    return "#{RPC}?type=search#{single_search_arg(term)}"   if type == :search
+    def fetch_json(url)
+      results = []
 
-    nil
-  end
+      resp = Net::HTTP.get_response(URI.parse(url))
+      json = JSON.parse(resp.body)
 
-  # downloads json_url and parses as json, results are discarded if a
-  # block is given which returns false for the result hash. finally,
-  # from_json_result is called and its result added to the return array.
-  def init_from_json(*args)
-    results = []
+      return results unless json.has_key?(KEY) 
+      return results if (jrs = json[KEY]).class != Array 
+      return results if jrs.empty?
 
-    # unable to determine url
-    return results unless url = json_url(*args)
+      jrs.threaded_each do |jr|
+        result = yield jr
+        results << result if result
+      end
 
-    resp = Net::HTTP.get_response(URI.parse(url))
-    json = JSON.parse(resp.body)
-
-    # no results
-    return results unless json.has_key?(KEY) 
-    return results if (jrs = json[KEY]).class != Array 
-    return results if jrs.empty?
-
-    jrs.threaded_each do |jr|
-      next if block_given? && !yield(jr)
-      result = from_json_result(jr)
-      results << result if result
+      results
     end
 
-    results
-  end
+    def url_for_search(term)
+      'http://aur.archlinux.org/rpc.php' +
+        '?type=search&arg=' + CGI::escape(term)
 
-  private
+    end
 
-  def single_search_arg(term)
-    '&arg=' + CGI::escape(term)
-  end
-
-  def multi_search_arg(term)
-    term.split(' ').collect { |t|
-      '&arg[]=' + CGI::escape(t)
-    }.join
+    def url_for_info(terms)
+      'http://aur.archlinux.org/rpc.php?type=multiinfo' +
+        terms.collect { |t| '&arg[]=' + CGI::escape(t) }.join
+    end
   end
 end
